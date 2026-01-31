059718d6199d9629dc0bdfe14c3e6b63d6de5401
mirny20 <urbanvo@proton.me>
2026-01-31 13:36:36 +0200

    refactoring set_words test and word_storage logic

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from playwright.sync_api import Browser, Playwright, Page
 
 from pom.auth_page import AuthPage
 from pom.home_page import HomePage
-from words.word_provider import WordProvider
+from words import word_storage
 
 USERNAME_MAIN_USER = os.environ['USERNAME_MAIN_USER']
 PASSWORD_MAIN_USER = os.environ['PASSWORD_MAIN_USER']
@@ -25,6 +25,8 @@ def base_url():
 def pytest_runtest_makereport(item, call):
     outcome = yield
     rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
 
     if rep.when != "call" or rep.outcome == "passed":
         return
@@ -92,7 +94,13 @@ def anon_page(browser: Browser, base_url):
     context.close()
 
 
-@pytest.fixture(scope="function")
-def generated_word() -> dict[str, str]:
-    word_provider = WordProvider()
-    return word_provider.get_word_with_translation()
+@pytest.fixture
+def temp_word_lifecycle(request):
+    yield
+
+    if request.node.rep_call.passed:
+        temp_word = word_storage.load_word_from_temp_word_file()
+        if temp_word:
+            word_storage.add_word(temp_word)
+
+    word_storage.clear_temp_word_file()
--- words/word_storage.py
+++ words/word_storage.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 FILE_PATH = Path(__file__).parent / "data" / "words.json"
+TEMP_WORD_FILE_PATH = Path(__file__).parent / "data" / "temp_word.json"
 
 
 def load_words_from_file() -> list[dict]:
@@ -28,3 +29,37 @@ def add_word(word: dict):
     data = load_words_from_file()
     data.append(word)
     save_words_to_file(data)
+
+
+def load_word_from_temp_word_file() -> dict:
+    if not TEMP_WORD_FILE_PATH.exists():
+        return {}
+
+    with open(TEMP_WORD_FILE_PATH, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def save_temp_eng_word(word: str):
+    with open(TEMP_WORD_FILE_PATH, "w", encoding="utf-8") as file:
+        json.dump(
+            {
+                "word_eng": word,
+                "translation": None
+            },
+            file,
+            ensure_ascii=False,
+            indent=2
+        )
+
+
+def save_temp_translation(translation: str):
+    data = load_word_from_temp_word_file()
+    data["translation"] = translation
+
+    with open(TEMP_WORD_FILE_PATH, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
+
+
+def clear_temp_word_file():
+    with open(TEMP_WORD_FILE_PATH, "w", encoding="utf-8") as file:
+        json.dump({}, file, ensure_ascii=False, indent=2)
